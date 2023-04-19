0c47b3c1130776531bdd542e1c79941decf0dea6
Babi <Babi@DESKTOP-5CAPTV4>
2023-04-18 21:34:38 -0300

    Comitando inicio de cadastro

--- Projeto2.0/src/bancoDeDados/dadosFilhos.java
+++ Projeto2.0/src/bancoDeDados/dadosFilhos.java
@@ -1,5 +1,23 @@
 package bancoDeDados;
 
 public class dadosFilhos {
+	
+	String nome;
+	int idade;
+	String dataNascimento;
+	String endereco;
+	String registroGeral;
+	double altura;
+	String numeroCpf;
+	String nomeMae;
+	String nomePai;
+	String dataEntradaTerreiro;
+	String terreiroAntigo;
+	String incorporacao;
+	String guiaFrente;
+	String orixasCabeca;
+	String mensalidade;
+	String diaDeGira;
+	
 
 }
--- Projeto2.0/src/terreiro/filhosDaCasa.java
+++ Projeto2.0/src/terreiro/filhosDaCasa.java
@@ -1,9 +1,28 @@
 package terreiro;
 
+import javax.swing.JOptionPane;
+
 public class filhosDaCasa {
 	
 	
 	
-	
+	public static void main(String[] args) {
+		
+		String nome = JOptionPane.showInputDialog("Qual o nome?");
+		String idade = JOptionPane.showInputDialog("Qual a idade?");
+		String dataNascimento = JOptionPane.showInputDialog("Qual a data de nascimento?");
+		String endereco = JOptionPane.showInputDialog("Qual seu endereço?");
+		String registroGeral = JOptionPane.showInputDialog("Qual o RG?");
+		String altura = JOptionPane.showInputDialog("Qual sua altura?");
+		String numeroCpf = JOptionPane.showInputDialog("Qual o CPF?");
+		String nomeMae = JOptionPane.showInputDialog("Qual o nome da mãe?");
+		String nomePai = JOptionPane.showInputDialog("Qual o nome da Pai?");
+		String dataEntradaTerreiro = JOptionPane.showInputDialog("Que dia está realizando esse cadastro?");
+		String terreiroAntigo = JOptionPane.showInputDialog("Você era de algum terreiro?Se sim, qual?");
+		String incorporacao = JOptionPane.showInputDialog("Você já incorpora alguma linha?Quais?");
+		String guiaFrente = JOptionPane.showInputDialog("Qual seu guia de frente?");
+		String orixasCabeca = JOptionPane.showInputDialog("Quais seus Orixás de cabeça?");
+		
+	}
 
 }
